50651be6b9c709382b18a65efed6b9671ca8dc0d
hiroki1600 <hiroki9452@gmail.com>
2020-12-02 13:47:00 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, expect: [:index]
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+ private
+
+  def move_to_index
+    unless user_sign_in?
+      redirect_to action: :index
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
 
+  validates :name, presence: true, 
+                   length: {maximum: 40 }
+  validates :text, presence: true
+  validates :category, presence: true
+  validates :status, presence: true
+  validates :shpping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: :true
+  validates :price, presence: true,
+                    format: { with: /\A[0-9]+\z/ }, 
+                    numericality: { in: 300..9999999 }
+
+
   belongs_to :item
 
 end
