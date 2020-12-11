0844e5ea41810ff5246114d3c7d77bfd72bfc4a4
hiroki1600 <hiroki9452@gmail.com>
2020-12-11 15:30:14 +0900

    クレカ登録実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,15 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :create]
   before_action :set_item, only: [:index, :create]
 
   def index
-    @order_address = OrderAddress.new
+       @order_address = OrderAddress.new
   end
 
   def create
-    @order_address = OrderAddress.new(order_address)
+       @order_address = OrderAddress.new(order_address)
     if @order_address.valid?
+       pay_item
        @order_address.save
        return redirect_to root_path
     else
@@ -18,12 +19,21 @@ class OrdersController < ApplicationController
 
   private
 
-  def order_address
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-  end
-
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_address
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  attr_accessor :token
+
+  validates :token, presence: true
 
   has_one :address
   belongs_to :user
