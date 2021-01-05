1f3a8655c7d55b06f3c9fb2007205d2593ef079f
hiroki1600 <hiroki9452@gmail.com>
2021-01-05 21:58:45 +0900

    SNSで新規登録

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,6 +18,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     else
       @sns_id = sns_info[:sns].id
+      binding.pry
       render template: 'devise/registrations/new'
     end
   end
