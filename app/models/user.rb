75d614e449b669c05066c9d67da78d33ce665233
hiroki1600 <hiroki9452@gmail.com>
2021-01-05 20:32:33 +0900

    残りHTMLのみ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
    devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
          
    validates :nickname, presence: true, length: { maximum: 40 }
    validates :birthday, presence: true
@@ -18,9 +19,26 @@ class User < ApplicationRecord
  has_many :items
  has_many :favorites, dependent: :destroy
  has_many :messages
+ has_many :sns_credentials
 
  def already_favorited?(item)
   self.favorites.exists?(item_id: item.id)
  end
+
+ def self.from_omniauth(auth)
+  sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+  # sns認証したことがあればアソシエーションで取得
+  # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+  user = User.where(email: auth.info.email).first_or_initialize(
+    nickname: auth.info.name,
+      email: auth.info.email
+  )
+  # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+  if user.persisted?
+    sns.user = user
+    sns.save
+  end
+  { user: user, sns: sns }
+  end
 
 end
